3d06a5cd708178b7ed5a456bf501917566082f69
annetlevitskaya <annetlevitskaya@gmail.com>
2018-11-30 23:57:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// function initMap() {
+//     var mapProp= {
+//         center: new google.maps.LatLng(48.104257,17.097853),
+//         zoom:18,
+//         zoomControl: false,
+//         mapTypeControl: false,
+//         scaleControl: false,
+//         streetViewControl: false,
+//         rotateControl: false,
+//         fullscreenControl: false
+//     };
+
+//     new google.maps.Map(document.getElementById("map"), mapProp);
+// }
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGZ1bmN0aW9uIGluaXRNYXAoKSB7XHJcbi8vICAgICB2YXIgbWFwUHJvcD0ge1xyXG4vLyAgICAgICAgIGNlbnRlcjogbmV3IGdvb2dsZS5tYXBzLkxhdExuZyg0OC4xMDQyNTcsMTcuMDk3ODUzKSxcclxuLy8gICAgICAgICB6b29tOjE4LFxyXG4vLyAgICAgICAgIHpvb21Db250cm9sOiBmYWxzZSxcclxuLy8gICAgICAgICBtYXBUeXBlQ29udHJvbDogZmFsc2UsXHJcbi8vICAgICAgICAgc2NhbGVDb250cm9sOiBmYWxzZSxcclxuLy8gICAgICAgICBzdHJlZXRWaWV3Q29udHJvbDogZmFsc2UsXHJcbi8vICAgICAgICAgcm90YXRlQ29udHJvbDogZmFsc2UsXHJcbi8vICAgICAgICAgZnVsbHNjcmVlbkNvbnRyb2w6IGZhbHNlXHJcbi8vICAgICB9O1xyXG5cclxuLy8gICAgIG5ldyBnb29nbGUubWFwcy5NYXAoZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJtYXBcIiksIG1hcFByb3ApO1xyXG4vLyB9Il0sImZpbGUiOiJtYWluLmpzIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
